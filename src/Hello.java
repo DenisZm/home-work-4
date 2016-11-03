a4b95722399ce724b73916b58439a4da5f4d0490
Denis Zamosenchuk <denis.zamosenchuk@gmail.com>
2016-11-03 19:16:51 +0200

    Second commit

--- src/Hello.java
+++ src/Hello.java
@@ -1,8 +1,9 @@
 /**
  * Created by Denis on 31.10.2016.
  */
+
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, Git!");
     }
 }
